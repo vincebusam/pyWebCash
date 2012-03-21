857c25e31f7fa0a0da0ba074167544f1a7e8d5f5
Vince Busam <vince@sixpak.org>
2012-03-21 01:57:05 -0700

    Correctly downloads all deposited check images.

--- webscrape.py
+++ webscrape.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import re
 import sys
 import time
 import getpass
@@ -6,8 +7,20 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import *
 
+def scrolluntilclick(b,e):
+    for retry in range(40):
+        try:
+            e.click()
+            return True
+        except:
+            b.execute_script("document.body.scrollTop=document.body.scrollTop+20;")
+            time.sleep(0.1)
+
 if len(sys.argv) < 2:
     sys.exit(1)
+
+datematch = re.compile("\d{2}/\d{2}/\d{4}")
+
 username = sys.argv[1]
 print "Enter password for %s: " % (username),
 password = getpass.getpass()
@@ -17,7 +30,8 @@ b.get("https://www.bankofamerica.com/")
 b.find_element_by_id("id").send_keys(username)
 Select(b.find_element_by_id("stateselect")).select_by_value("CA")
 b.find_element_by_id("top-button").click()
-time.sleep(5)
+while not b.find_elements_by_id("tlpvt-passcode-input"):
+    time.sleep(5)
 b.find_element_by_id("tlpvt-passcode-input").send_keys(password)
 b.find_element_by_name("confirm-sitekey-submit").click()
 accounts = {}
@@ -26,20 +40,28 @@ for a in b.find_elements_by_xpath("//div[contains(@class,'image-account')]/a"):
 for acct in accounts:
     b.get(accounts[acct])
     for loop in range(1000):
-        try:
-            b.find_element_by_id("row%s" % (loop))
-        except:
+        if not b.find_elements_by_id("row%s" % (loop)):
             break
         date = b.find_element_by_xpath("//tr[@id='row%s']/td[3]" % (loop)).text
-        desc = b.find_element_by_xpath("//tr[@id='row%s']/td[4]" % (loop)).text
+        if not datematch.match(date):
+            continue
+        desc = b.find_element_by_xpath("//tr[@id='row%s']/td[4]" % (loop)).text.replace("\n","")
         amount = b.find_element_by_xpath("//tr[@id='row%s']/td[7]" % (loop)).text
-        b.find_element_by_id("rtImg%i"%(loop)).click()
-        b.find_element_by_id("ViewImages").click()
-        for checkid in range(1,1000):
-            try:
+        print "Transaction: %s %s %s %s" % (acct,date,desc,amount)
+        if "BKOFAMERICA ATM DEPOSIT" in desc:
+            if not scrolluntilclick(b,b.find_element_by_id("rtImg%i"%(loop))):
+                continue
+            b.find_element_by_id("ViewImages").click()
+            for checkid in range(1,20):
+                if not b.find_elements_by_id("icon%s"%(checkid)):
+                    continue
                 b.find_element_by_id("icon%s"%(checkid)).text
-            except:
-                break
-            b.find_element_by_xpath("//td[@id='icon%s']/a/img"%(checkid)).click()
-            b.get(b.find_element_by_xpath("//td[@class='imageborder']/img").get_attribute("src"))
-            b.save_screenshot("check.png")
+                gotcheck = False
+                if not scrolluntilclick(b,b.find_element_by_xpath("//td[@id='icon%s']/a/img"%(checkid))):
+                    continue
+                b.get(b.find_element_by_xpath("//td[@class='imageborder']/img").get_attribute("src"))
+                b.save_screenshot(("%s-%s-%s.png"%(acct,date,checkid)).replace("/","_"))
+                b.back()
+b.find_element_by_link_text("Sign Off").click()
+time.sleep(5)
+b.close()
