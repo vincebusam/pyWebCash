fcb6f5af87694212185b7d604a6089e5aea4372d
Vince Busam <vince@sixpak.org>
2012-03-21 15:49:20 -0700

    Clean up cookie handling, add logout method.

--- web/api.py
+++ web/api.py
@@ -4,6 +4,7 @@ import sys
 import cgi
 import json
 import Cookie
+import datetime
 
 sys.path.append("../db")
 sys.path.append("../")
@@ -24,12 +25,15 @@ action = form.getfirst("action")
 username = form.getfirst("username")
 password = form.getfirst("password")
 if os.getenv("HTTP_COOKIE"):
-    cookies = Cookie.SimpleCookie()
-    cookies.load(os.getenv("HTTP_COOKIE"))
-    if not username:
-        username = cookies["username"].value
-    if not password:
-        password = cookies["password"].value
+    try:
+        cookies = Cookie.SimpleCookie()
+        cookies.load(os.getenv("HTTP_COOKIE"))
+        if not username:
+            username = cookies["username"].value
+        if not password:
+            password = cookies["password"].value
+    except (Cookie.CookieError, KeyError):
+        pass
 
 if not username or not password:
     exit_error(400,"incomplete username/password")
@@ -43,6 +47,17 @@ if action == "login":
     cookies = Cookie.SimpleCookie()
     cookies["username"] = username
     cookies["password"] = password
+    print "Content-type: application/json"
+    print cookies
+    print
+    print json.dumps(True)
+elif action == "logout":
+    expire = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%a, %d-%b-%Y %H:%M:%S PST")
+    cookies = Cookie.SimpleCookie()
+    cookies["username"] = ""
+    cookies["username"]["expires"] = expire
+    cookies["password"] = ""
+    cookies["password"]["expires"] = expire
     print "Content-type: application/json"
     print cookies
     print
