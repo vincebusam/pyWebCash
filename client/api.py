f8aa9a7971818926d1a8e0e7e95166add5b226a3
Vince Busam <vince@sixpak.org>
2012-03-21 16:49:02 -0700

    Beginning framework for scraper client.

---
+++ client/api.py
@@ -0,0 +1,18 @@
+import sys
+import json
+import urllib
+import urllib2
+import cookielib
+
+sys.path.append("../")
+
+import config
+
+cj = cookielib.CookieJar()
+
+def callapi(action, postdata={}):
+    postdata.update({"action": action})
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    f = opener.open(config.apiurl,urllib.urlencode(postdata))
+    data = f.read()
+    return json.loads(data)
---
+++ client/gettransactions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python2.6
+import sys
+import api
+import getpass
+
+print "Login"
+print "Username: ",
+username = sys.stdin.readline().strip()
+password = getpass.getpass()
+
+if not api.callapi("login",{"username": username, "password": password}):
+    print "Login failed"
+    sys.exit(1)
+
+todo = api.callapi("accountstodo")
+print todo
