4d3a8f679ba7fbc8813483f336774b986c6ab0f6
Vince Busam <vince@sixpak.org>
2012-03-22 11:42:42 -0700

    Don't pad a string that's a multiple of BLOCK_SIZE.

--- db/aesjsonfile.py
+++ db/aesjsonfile.py
@@ -7,7 +7,7 @@ from Crypto.Cipher import AES
 
 BLOCK_SIZE = 32
 PADDING = ' '
-pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
+pad = lambda s: s + (BLOCK_SIZE - ((len(s) % BLOCK_SIZE) or BLOCK_SIZE)) * PADDING
 EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
 DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
 
