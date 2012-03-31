5fad0585cf2e1412f137c5826baf688953c673db
Vince Busam <vince@sixpak.org>
2012-03-30 19:55:59 -0700

    Add CSV importer.

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ apiurl = "https://sixpak.org/finance/api.py"
 sessiondir = "/usr/local/finance/session"
 sessiontimeout = (60*15) # 15 minutes
 
-banks = [ "bankofamerica", "citicards" ]
+banks = [ "bankofamerica", "citicards", "csvurl" ]
