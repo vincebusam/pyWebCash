4284c0105ce7931c044256da0eaae072f602f723
Vince Busam <vince@sixpak.org>
2012-04-02 23:15:25 -0700

    Need to insert parent before children.

--- client/bankofamerica.py
+++ client/bankofamerica.py
@@ -75,6 +75,7 @@ def downloadaccount(b, params):
                     files[checkfn] = b.get_screenshot_as_base64()
                     b.back()
                     transaction["file"] = checkfn
+            newtransactions.append(transaction)
             if b.find_elements_by_id("ViewImages"):
                 common.scrolluntilclick(b,b.find_element_by_id("ViewImages"))
                 for checkid in range(1,20):
@@ -104,7 +105,6 @@ def downloadaccount(b, params):
                         subtrans["file"] = checkfn
                     newtransactions.append(subtrans)
                     transaction.setdefault("children",[]).append(subtrans["id"])
-            newtransactions.append(transaction)
         balance = b.find_element_by_class_name("module1bkgd13").text
         print "Balance %s %s" % (acct, balance)
         balances.append({"account": params["name"], "subaccount": acct, "balance": balance, "date": datetime.date.today()})
