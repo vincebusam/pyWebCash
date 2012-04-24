10d867550972b95bb995858bdfbd1359f642febe
Vince Busam <vince@sixpak.org>
2012-04-23 23:17:41 -0700

    Need certfile in config.

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ imgdir = "/usr/local/finance/img"
 apiurl = "https://example.com/pywebfinance/api.py"
 sessiondir = "/usr/local/finance/session"
 sessiontimeout = (60*15) # 15 minutes
+certfile = None
 
 banks = [ "bankofamerica", "citicards", "wellsfargo", "chase", "paypal", "gap", "fecu", "csvurl", "imap" ]
