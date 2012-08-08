c553a385090bcfefcac54d59025ab2a39f66c9ac
Vince Busam <vince@sixpak.org>
2012-08-07 21:58:48 -0700

    Skip paperless question.

--- client/provident.py
+++ client/provident.py
@@ -30,6 +30,8 @@ def downloadaccount(b, params):
     subaccount = b.find_element_by_id("ctl00_ctl00_cphCenter_cphContent__propertyStreetAddressLabel").text
     balance = "-" + b.find_element_by_id("ctl00_ctl00_cphCenter_cphContent__currLoanBalLabel").text
     balances = [{"account": params["name"], "subaccount": subaccount, "balance": balance, "date": datetime.date.today()}]
+    if b.find_elements_by_id("ctl00_ctl00_cphCenter_cphContent_lnkSignUpLater"):
+        b.find_elements_by_id("ctl00_ctl00_cphCenter_cphContent_lnkSignUpLater").click()
     b.find_element_by_link_text("Logout").click()
     return {"balances": balances}
 
